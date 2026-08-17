95abc97f0e4bf0a71bdd1fff5ca743b38ffbd97c
Haneesh <cholamhaneesh@gmail.com>
2026-08-17 20:15:30 +0530

    font color

--- ui/base_layout.py
+++ ui/base_layout.py
@@ -38,6 +38,7 @@ def style_base_layout():
                             font-size: 2rem !important;
                             line-height: 1.1 !important;
                             margin-bottom:0rem !important;
+                            color: #1E293B !important;
                         }
                         
                         h3, h4, p, span{
